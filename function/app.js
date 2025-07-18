49ba94b611f506cc7d6a6d39e1018787700b073b
2ma1995 <vthquddbsv@gmail.com>
2025-07-18 20:28:59 +0900

    함수 시작

---
+++ function/app.js
@@ -0,0 +1,15 @@
+const startGameBtn = document.getElementById('start-game-btn');
+
+function startGame() {
+    console.log('game started');
+}
+
+// const person = {
+//     greet: function greet() {
+//         console.log('hello world ');
+//     },
+// };
+//
+// person.greet();
+
+startGameBtn.addEventListener('click', startGame);
